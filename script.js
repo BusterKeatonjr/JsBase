05e1d6173be6b522b48ff69e72cb6cfe35f49546
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-07 23:27:18 +0300

    Строки в JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 "use strict";
-let a = 2;
-let b = 10;
-alert(a ** b)
-let q = 3 * 2 ** 3;
-alert(q);//24
-let w = (3 * 2) ** 3;
-alert(w);//216
-let e = 3 * 2 ** (3 + 1);
-alert(e);//48
-let r = 2 ** 3 * 3;
-alert(r);//24
-let t = 3 * 2 ** 3 * 3;
-alert(t);//72
+let a = 'Evgeniy';
+let b = 'Tatarenkov';
+alert(a);
+alert(b);
+
