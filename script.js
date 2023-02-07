a67abc021435bccbc5da4eca6d9477e6f217db1b
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-07 23:12:27 +0300

    Приоритет математических операций в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 "use strict";
-let a= 10;
-let b = 5;
-let c = a - b;
-alert(c)
-let d = 7;
-let result = c + d;
-alert(result)
+let q = 5 + 5 * 3;
+alert(q); // 20
+let a = 5 + 5 * 3 + 3;
+alert(a)// 23
+let w = 8 / 2 + 2;
+alert(w);// 6
+let e = 8 + 2 / 2;
+alert(e);//9 
+let r = 8 / 2 * 2;
+alert(r);//8
+let t = 8 * 4 / 2 / 2;
+alert(t);//8
+let y = (2 + 3) * (2 + 3);
+alert(y);//25
+let u = (2 + 3) * 2 + 3;
+alert(u);//13
+let i = 2 * (2 + 4 * (3 + 1));
+alert(i);//36
+let o = 2 * 8 / 4;
+alert(o);//4
+let p = (2 * 8) / 4;
+alert(p);//4
+let s = 2 * (8 / 4);
+alert(s);//4
