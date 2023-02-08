a830f776424a2e6540a79643347378b7d447a1a1
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-08 17:31:46 +0300

    Работа с консолью в JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 "use strict";
-console.log(123);
-
-let num1 = 3, num2 = 4, num3 = 5;
-console.log(num1, num2, num3);
-
-let a = 'a', b = true, c = false, d = null, e = 1;
-console.log(a,b,c,d,e);
-
-alert(eeeeeeeeeeeeeeeeeeeeeeeee);
+const PI = 3.14;
+let r = 5;
+let a = 2 * PI *  r;
+alert(a)
 
 
 
