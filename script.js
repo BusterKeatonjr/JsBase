db24bcded1d027d95732427d259b482236ffedf2
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-08 17:27:32 +0300

    Специальные значения в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 "use strict";
-let str = `a
-b
-c`;
+let str ;
 alert(str);
+
+let a = null;
+alert(a);
+
+let b = true;
+alert(b);
+
+let c = false;
+alert(c);
+
+let d = 'ab';
+let e = 'oba';
+alert('d' * 'e');
+
+let f = 10;
+alert(f / 0);
+
+let g = -10;
+alert(g / 0);
 
 
 
