140e4cb8cf45901506198452d7154d0a88977ff9
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:45:18 +0300

    Длина массива в JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 "use strict";
-let arr_num = [1, 2, 3],
-  arr_str = ["a", "b", "c", "d"];
-document.write(
-  ` ${arr_num[0]}<br>${arr_num[1]}<br>${arr_num[2]}<br>`
-);
-document.write(`: ${arr_num[0]}, ${arr_num[1]}, ${arr_num[2]}<br>`);
-document.write(` ${arr_num[0] + arr_num[1] + arr_num[2]}<br>`);
-document.write(
-  ` ${arr_str[0]}+${arr_str[1]}+${arr_str[2]}+${arr_str[3]}`
-);
+let arr = ['1','2','3']
+document.write(`${arr.length}<br>`)
+
+let ar = [1, 2, 5];
+document.write(arr[arr.length - 1])
