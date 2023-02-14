46d2080b52c378e35bbc436a1bc9372504b72c92
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 19:11:22 +0300

    Свойства объектов в JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 "use strict";
-let user = { name: "Peter", surname: "Parker", patronymic: "Spider Man" };
-document.write(`${user.name} ${user.surname} ${user.patronymic}`);
+let today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0");
+var yyyy = today.getFullYear();
+console.log(yyyy);
+let date = { year: yyyy, month: mm, day: dd };
+document.write(`${date.year}-${date.month}-${date.day}`);
