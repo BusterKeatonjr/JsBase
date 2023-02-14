000f329bcaa0bffa7ffe6d6cc967e2d75f0f671c
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:37:39 +0300

    Практика на формулы в JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
-document.write(` ${24 * 60 * 60}<br>`);
-document.write(` ${30 * 24 * 60 * 60}<br>`);
-document.write(` ${365 * 24 * 60 * 60}<br>`);
-document.write(` ${24 * 60}<br>`);
-document.write(` ${365 * 24 * 60}<br>`);
-document.write(` ${1024 * 1024}<br>`);
-document.write(` ${1024 * 1024 * 1024}<br>`);
-document.write(` ${1024 * 1024 * 1024 * 10}<br>`);
-document.write(` ${1024 * 1024 * 1000}<br>`);
-document.write(` ${1024 * 1024 * 1024 * 1000}<br>`);
-
+let r_circle = Number(prompt("Введите радиус круга: ")),
+  Pi = Math.PI;
+document.write(` ${Pi * r_circle ** 2}`);
+let a_square = Number(prompt("Введите сторону квадарата: "));
+document.write(`<br> ${a_square ** 2}`);
+let l_rect = Number(prompt("Введите сторону прямоугольника: ")),
+  w_rect = Number(prompt("Введите вторую сторону: "));
+document.write(`<br> ${w_rect * l_rect}`);
+document.write(`<br> ${2 * (l_rect + w_rect)}`);
+let tc = Number(prompt("Введите показатель: "));
+document.write(`<br> ${tc * (9 / 5) + 32}`);
+let fc = Number(prompt("Введите показатель: "));
+document.write(`<br> ${(fc - 32) / (9 / 5)}`);
