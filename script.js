bf8959ddd86ed33ad87d9a03fe6c51e92ba1067b
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:56:37 +0300

    Добавление элементов через push в JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
 let arr = [];
-
-arr[3] = "a";
-arr[8] = "b";
-document.write(`Длина: ${arr.length}`);
+for (let index = 0; index < 3; index++) {
+  arr.push(index + 1);
+}
+document.write(` ${arr}<br>`);
+for (let index = 3; index < 5; index++) {
+  arr.push(index + 1);
+}
+document.write( arr);
 
