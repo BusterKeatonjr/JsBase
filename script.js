b6e39edf067c4d45bb984e681f207ac5922b4e43
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:11:44 +0300

    Функция prompt в JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 "use strict";
-alert(0.1 * 0.2); // 0.020000000000000004
- 
-alert(0.3 - 0.1); // 0.19999999999999998
+let name = prompt('Ваш возраст'); // задание 1
+alert('Ваш возраст: ' + name);
+
+let num1 = Number(prompt('Введите первое число')); // задание 2
+let num2 = Number(prompt('Введите второе число'));
+
+alert(num1 + num2);
+
+let a = Number(prompt('Введите сторону квадрата'));// задание 3
+let b = 4
+
+alert(a * b)
+
+let c = Number(prompt('Введите первую сторону прямоугольника'));// задание 4
+let d = Number(prompt('Введите вторую сторону прямоугольника'));
+let v = 2
+
+alert(c * v + d * v)
