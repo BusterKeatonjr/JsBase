fdfb3f17832b8d462170924aa391ebe7ab4943e2
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:53:19 +0300

    Добавление элементов по ключам JavaScript

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 "use strict";
-let arr = ['a', 'b', 'c'];
-
-arr[0] = '1';
-arr[1] = '2';
-arr[2] = '3';
-document.write(`${arr}<br>`); 
-
-let ar = [1, 2, 3];
-
-ar[0] += 3;
-ar[1] += 3;
-ar[2] += 3;
-
-document.write(`${ar}<br>`);
-
-let arrr = [1, 2, 3, ];
-
-arrr[0]++;
-++arrr[1];
-arrr[2]++;
-
-
-document.write(arrr);
+let arr = [];
+(arr[0] = 1), (arr[1] = 2), (arr[2] = 3);
+document.write(` ${arr}<br>`);
+(arr[3] = 4), (arr[4] = 5);
+document.write(arr);
 
 
