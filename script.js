61ed81b5f4389aee81a2dcd0539fab71c66b8aa8
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 16:28:19 +0300

    Строки с цифрами в JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 "use strict";
+let test = '12345';
+alert(test[0] + test[1]);
+
 let a = String(12345); 
-alert(a[0] + a[1] + a[2] + a[3] + a[4]);
+alert(a[0] + a[1]);
+alert(a[0] * a[1] * a[2] * a[3]* a[4] );
 
 let b = String(12345); 
-alert(b[0] * b[1] * b[2] * b[3] * b[4]);
-
-let c = String(12345); 
-alert(c[4] + c[3] + c[2] + c[1] + c[0]);
-
-
+alert(b[4],b[3],b[2],b[1],b[0] );
