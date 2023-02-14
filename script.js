198a232e19ac0a7d3fa0f20c4a5b53b37b8b0b5a
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:59:54 +0300

    Оператор delete в массивах в JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict";
-let arr = ["a", "b", "c"],
-  array = [1, 2, 3, 4, 5],
-  key1 = 1,
-  key2 = 2;
+let arr = ["a", "b", "c", "d", "e"];
 
-document.write(` ${arr[key2]}<br>`);
-document.write(array[key1] + array[key2]);
-
+delete arr[1], delete arr[3];
+document.write(arr.length );
