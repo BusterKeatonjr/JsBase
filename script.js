3cf3ce0af30646c1c4fad330e512c1949fca4d0c
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:33:27 +0300

    Практика на операции в JavaScript

--- script.js
+++ script.js
@@ -1,54 +1,12 @@
 "use strict";
-let num1 = 1;
-let num2 = 2;
-console.log(`сумма: ${num1 + num2}`);
-
-let a = 1;
-let b = 2;
-console.log(`${a + b}`);
-
-let num = '123';
-let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
-console.log(sum);
-
-let num_new = 123;
-num_new = String(num_new);
-console.log(num_new[0]);
-
-let c = 0;
-console.log(++c);
-
-let num_new_new = 123;
-num_new_new = String(num_new_new);
-console.log(num_new_new.length);
-
-let d = 24 * 60 * 60;
-console.log(d);
-
-let num_bebra = 123;
-let str = String(num_bebra);
-console.log(str.length);
-
-let number = 123;
-let string = String(number);
-console.log(Number(string[string.length - 1]));
-
-let num_HVATIT = 123;
-let str_HVATIT = String(num_HVATIT);
-console.log(str_HVATIT.length);
-
-let num_AAAAAGH = 123;
-let str_AAAAAGH = String(num_AAAAAGH);
-console.log(str_AAAAAGH[str_AAAAAGH.length - 1]);
-
-let e = '123';
-let g = '456';
-let s = Number(e) + Number(g);
-console.log(s);
-
-let aaa = 1;
-let bbb = 2;
-let ccc = 3;
-
-console.log(aaa + bbb + ccc);
+document.write(` ${24 * 60 * 60}<br>`);
+document.write(` ${30 * 24 * 60 * 60}<br>`);
+document.write(` ${365 * 24 * 60 * 60}<br>`);
+document.write(` ${24 * 60}<br>`);
+document.write(` ${365 * 24 * 60}<br>`);
+document.write(` ${1024 * 1024}<br>`);
+document.write(` ${1024 * 1024 * 1024}<br>`);
+document.write(` ${1024 * 1024 * 1024 * 10}<br>`);
+document.write(` ${1024 * 1024 * 1000}<br>`);
+document.write(` ${1024 * 1024 * 1024 * 1000}<br>`);
 
