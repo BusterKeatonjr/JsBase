178c1c14385cea9656cade8b03edb96fb65c26a6
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 16:40:19 +0300

    Неточные вычисления в JavaScript

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
 "use strict";
-let num = 10;
-num  ++ ;
-num ++;
-num --;
-alert(num);//11
-let num3 = 3;
-alert(++num3); //4
-let num4 = 3;
-alert(num4++);//3
-let num5 = 3;
-alert(--num5);//2
-let num6 = 3;
-alert(num6--);//3
-let num10 = 3;
-let num20 = ++num10;
-alert(num10);//4
-alert(num20);//4
-let num11 = 3;
-let num21 = num11++;
-alert(num11);//4
-alert(num21);//3
-let num12 = 3;
-let num22 = --num12;
-alert(num12);//2
-alert(num22);//2
-let num13 = 3;
-let num23 = num13--;
-alert(num13);//2
-alert(num23);//3
-let num14 = 3;
-num14++;
-let num24 = num14--;
-alert(num14++);//3
-alert(--num24);//3
+alert(0.1 * 0.2); // 0.020000000000000004
+ 
+alert(0.3 - 0.1); // 0.19999999999999998
