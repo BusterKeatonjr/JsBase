e1ee7c74d0c516e49f25267d3d0ecf974362ea3d
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-14 18:52:08 +0300

    Изменение элементов массива в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 "use strict";
-let arr = ['1','2','3']
-document.write(`${arr.length}<br>`)
+let arr = ['a', 'b', 'c'];
 
-let ar = [1, 2, 5];
-document.write(arr[arr.length - 1])
+arr[0] = '1';
+arr[1] = '2';
+arr[2] = '3';
+document.write(`${arr}<br>`); 
+
+let ar = [1, 2, 3];
+
+ar[0] += 3;
+ar[1] += 3;
+ar[2] += 3;
+
+document.write(`${ar}<br>`);
+
+let arrr = [1, 2, 3, ];
+
+arrr[0]++;
+++arrr[1];
+arrr[2]++;
+
+
+document.write(arrr);
+
+
