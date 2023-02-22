48b3f5a048040783d4c6b2c3c5bd58b3cc51f893
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-22 17:46:08 +0300

    72

--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let obj = { x: 1, y: 2, z: 3 };
-console.log(obj.x);
-let key = "x";
-console.log(obj[key]);
-let sum = obj.x + obj.y + obj.x;
-console.log(sum);
-console.log(Object.keys(obj).length);
