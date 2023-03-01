aa417caf545ee3ac5c02d772edbf356739ff0c7c
BusterKeatonjr <evgexa0007@gmail.com>
2023-03-01 16:30:58 +0300

    99

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 "use strict";
 
-let a = 2 * (3 - 1);
-let b = 6 - 2;
-document.write(a == b, '<br>')
+let ok = confirm('Вам есть 18 лет?');
 
-let c = 5 * (7 - 4);
-let d = 1 + 2 + 7;
-document.write(c > d, '<br>')
-
-let e = 2 ** 4;
-let f = 4 ** 2;
-document.write(e != f)
+if (ok) {
+	document.write('текст для взрослых 0_0');
+} else {
+	document.write('доступ запрещен');
+}
